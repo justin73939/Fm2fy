4eb428ce2f26e52ea6d65cc9a5206bd929b18666
justin73939 <147675535+justin73939@users.noreply.github.com>
2025-01-21 10:48:30 -0600

    [redacted]

--- Backend/config.py
+++
@@ -1,8 +0,0 @@
-#lastfm API credentials
-API_KEY = 'cd4dd321256fbdd9f2d13a7296643cda'  # Add your Last.fm API key
-USERNAME = 'Justin73939'  # Add your Last.fm username
-
-#Spotify API credentials
-SPOTIPY_CLIENT_ID = '02a24f8e54e94d0da84717290d7281c0'        # Add your Spotify client ID
-SPOTIPY_CLIENT_SECRET = 'a2e9cc1a0fb04095a175ff1419e14154'    # Add your Spotify client secret
-SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'     # Add your redirect URI
